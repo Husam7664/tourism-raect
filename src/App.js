0bbc6c490d8702d3fcaae2c704cea42ad6c6ad2d
Husam7664 <husam7664@gmail.com>
2023-04-16 15:17:29 +0530

    home serach bar styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './app.css'
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Components/Home/Home'
+import Popular from './Components/Popular/Popular'
 import Ticketing from './Components/Ticketing/Ticketing'
 import Workancy from './Components/Workancy/Workancy'
 import Footer from './Components/Footer/Footer'
@@ -11,6 +12,7 @@ const App = () => {
     <>
       <Navbar />
       <Home />
+      <Popular />
       {/* <Ticketing />
       <Workancy />
       <Footer /> */}
---
+++ src/Components/Popular/Popular.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './popular.css'
+
+const Popular = () => {
+  return (
+    <section className='popular section container'>
+      <div className='secContainer'>
+        <div className='secHeader flex'>
+          <div className='textDiv'>
+            <h2 className='secTitle'>Popular Desnination</h2>
+            <p>
+              From historical cities to natural specteurals, come see the best
+              of the world !
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Popular
